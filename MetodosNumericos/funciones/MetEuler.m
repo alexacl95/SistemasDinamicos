af9448126cb2cefece42b7123078ee26c1b4ea4b
alexacl95 <alexacl95@gmail.com>
2022-02-13 20:35:06 -0500

    Fix time in euler method

--- MetodosNumericos/funciones/MetEuler.m
+++ MetodosNumericos/funciones/MetEuler.m
@@ -1,11 +1,13 @@
 function y= MetEuler(F,condInit,time,h)
 
 Tsize=time/h;
-x = h:h:time;
+x = zeros(Tsize, 1);
+x(1) = 0;
 y = zeros(length(condInit),Tsize);
 y(:,1) = condInit;
 
     for i=1:Tsize-1
-        y(:,i+1) = y(:,i) + h*F(x(i),y(:,i));
+        x(i + 1) = x(i) + h;
+        y(:,i + 1) = y(:,i) + h*F(x(i),y(:,i));
     end
 end
